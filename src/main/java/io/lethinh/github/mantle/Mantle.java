d92ebdf8aadbd9fa9c497095627cda1dbdae126c
banbeucmas <banbeucams@gmail.com>
2018-07-23 20:57:31 +0000

    Rebase Source Structure

--- src/main/java/io/lethinh/github/mantle/Mantle.java
+++ src/main/java/io/lethinh/github/mantle/Mantle.java
@@ -17,52 +17,52 @@ import io.lethinh.github.mantle.loader.ILoader;
  */
 public class Mantle extends JavaPlugin {
 
-	public static final String PLUGIN_ID = "mantle"; // Just for enforcements, no conflicts with other plugins
+    public static final String PLUGIN_ID = "mantle"; // Just for enforcements, no conflicts with other plugins
 
-	private EventLoader eventLoader;
-	private CommandLoader commandLoader;
+    private EventLoader eventLoader;
+    private CommandLoader commandLoader;
 
-	@Override
-	public void onEnable() {
-		// Welcome message
-		Logger logger = getLogger();
-		logger.info(ChatColor.GOLD + "Mantle -" + ChatColor.LIGHT_PURPLE + " an agricultural with tech involvements"
-				+ ChatColor.WHITE + " is starting...");
+    @Override
+    public void onEnable() {
+        // Welcome message
+        Logger logger = getLogger();
+        logger.info(ChatColor.GOLD + "Mantle -" + ChatColor.LIGHT_PURPLE + " an agricultural with tech involvements"
+                + ChatColor.WHITE + " is starting...");
 
-		// Load
-		eventLoader = new EventLoader();
-		commandLoader = new CommandLoader();
+        // Load
+        eventLoader = new EventLoader();
+        commandLoader = new CommandLoader();
 
-		ILoader.LOADERS.forEach(loader -> {
-			try {
-				loader.load(this);
-			} catch (Throwable e) {
-				e.printStackTrace();
-				logger.warning("Failed to load " + loader.getClass().getSimpleName() + "! Plugin will not work well.");
-			}
-		});
+        ILoader.LOADERS.forEach(loader -> {
+            try {
+                loader.load(this);
+            } catch (Throwable e) {
+                e.printStackTrace();
+                logger.warning("Failed to load " + loader.getClass().getSimpleName() + "! Plugin will not work well.");
+            }
+        });
 
-		// Machine tick
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				CopyOnWriteArrayList<BlockMachine> machines = BlockMachine.MACHINES;
+        // Machine tick
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                CopyOnWriteArrayList<BlockMachine> machines = BlockMachine.MACHINES;
 
-				if (machines.isEmpty()) {
-					return;
-				}
+                if (machines.isEmpty()) {
+                    return;
+                }
 
-				machines.forEach(machine -> machine.tick(Mantle.this));
-			}
-		}.runTaskTimer(this, 20L, 20L);
-	}
+                machines.forEach(machine -> machine.tick(Mantle.this));
+            }
+        }.runTaskTimer(this, 20L, 20L);
+    }
 
-	public EventLoader getEventLoader() {
-		return eventLoader;
-	}
+    public EventLoader getEventLoader() {
+        return eventLoader;
+    }
 
-	public CommandLoader getCommandLoader() {
-		return commandLoader;
-	}
+    public CommandLoader getCommandLoader() {
+        return commandLoader;
+    }
 
 }
