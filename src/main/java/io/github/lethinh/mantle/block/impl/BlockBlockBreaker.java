bc7ef8ae4e2dd649478bb6b99faff84f21229aa7
Anh Craft <huynhduyanh123123@gmail.com>
2018-08-18 14:42:26 +0700

    Add physics support

--- src/main/java/io/github/lethinh/mantle/block/impl/BlockBlockBreaker.java
+++ src/main/java/io/github/lethinh/mantle/block/impl/BlockBlockBreaker.java
@@ -74,7 +74,7 @@ public class BlockBlockBreaker extends BlockMachineEnergized {
         }
 
         surround.getDrops().forEach(inventory::addItem);
-        surround.setType(Material.AIR);
+        surround.setType(Material.AIR, Mantle.instance.getConfig().getBoolean("performance.apply_physics"));
     }
 
     @Override
